5ae8d242ae0a6b34712f03666420ff0366b91f8d
vincentochoa3 <vochoa893@gmail.com>
2023-06-23 12:51:22 -0700

    add navbar to root layout

--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-[calc(100vh-4rem)] flex-col items-center justify-between p-4">
       <div className="w-full max-w-5xl items-center justify-between text-sm lg:flex">
-        <h2>Welcome to Quoted</h2>
+        <h1>Welcome to Quoted</h1>
       </div>
     </main>
   );
---
+++ components/Navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="w-full h-16 flex justify-between items-center bg-gray-900 p-4">
+      <div>
+        <a href="" className="font-bold">
+          "Quoted"
+        </a>
+      </div>
+      <div>
+        <a href="" className="font-semibold">
+          Login
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
